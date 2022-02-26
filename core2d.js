e8e64fa67e51b578816d6f4749317c70a674d305
Diogo Eichert <diogo.eichert@yahoo.com>
2022-02-26 13:06:10 +0100

    Remove makeMap

--- core2d.js
+++ core2d.js
@@ -715,26 +715,26 @@
 		constructor(event) {
 			this._buffer = {};
 
-			this.keyToCommandMap = makeMap([
-				Key.UP, Command.UP,
-				Key.E, Command.UP,
-				Key.I, Command.UP,
-				Key.DOWN, Command.DOWN,
-				Key.D, Command.DOWN,
-				Key.K, Command.DOWN,
-				Key.LEFT, Command.LEFT,
-				Key.S, Command.LEFT,
-				Key.J, Command.LEFT,
-				Key.RIGHT, Command.RIGHT,
-				Key.F, Command.RIGHT,
-				Key.L, Command.RIGHT,
-				Key.SPACE, Command.A,
-				Key.ALT, Command.B,
-				Key.CTRL, Command.X,
-				Key.SHIFT, Command.Y,
-				Key.ENTER, Command.START,
-				Key.BACK, Command.SELECT,
-			]);
+			this.keyToCommandMap = {
+				[Key.UP]: Command.UP,
+				[Key.E]: Command.UP,
+				[Key.I]: Command.UP,
+				[Key.DOWN]: Command.DOWN,
+				[Key.D]: Command.DOWN,
+				[Key.K]: Command.DOWN,
+				[Key.LEFT]: Command.LEFT,
+				[Key.S]: Command.LEFT,
+				[Key.J]: Command.LEFT,
+				[Key.RIGHT]: Command.RIGHT,
+				[Key.F]: Command.RIGHT,
+				[Key.L]: Command.RIGHT,
+				[Key.SPACE]: Command.A,
+				[Key.ALT]: Command.B,
+				[Key.CTRL]: Command.X,
+				[Key.SHIFT]: Command.Y,
+				[Key.ENTER]: Command.START,
+				[Key.BACK]: Command.SELECT,
+			};
 
 			this.onKey(event, true);
 			addEventListener("keydown", event => this.onKey(event, true), false);
@@ -2185,16 +2185,6 @@
 		}
 
 		return RESULT;
-	}
-
-	function makeMap(array) {
-		const result = {};
-
-		for (let i = 0; i < array.length; ++i) {
-			result[array[i]] = array[++i];
-		}
-
-		return result;
 	}
 
 	function toDegrees(radians) {

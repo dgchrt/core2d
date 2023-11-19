c14411dad818f81100f35f160e6f2a92fa48b91d
Diogo Eichert <diogoeichert@icloud.com>
2023-11-19 13:17:33 +0100

    Extract KeyMap

--- src/Keyboard.mjs
+++ src/Keyboard.mjs
@@ -1,32 +1,10 @@
 "use sttrict";
 
-import { Command } from "./Command.mjs";
-import { Key } from "./Key.mjs";
+import { KeyMap } from "./KeyMap.mjs";
 
 export class Keyboard {
 	constructor(event) {
 		this._buffer = {};
-
-		this.keyToCommandMap = {
-			[Key.UP]: Command.UP,
-			[Key.E]: Command.UP,
-			[Key.I]: Command.UP,
-			[Key.DOWN]: Command.DOWN,
-			[Key.D]: Command.DOWN,
-			[Key.K]: Command.DOWN,
-			[Key.LEFT]: Command.LEFT,
-			[Key.S]: Command.LEFT,
-			[Key.J]: Command.LEFT,
-			[Key.RIGHT]: Command.RIGHT,
-			[Key.F]: Command.RIGHT,
-			[Key.L]: Command.RIGHT,
-			[Key.SPACE]: Command.A,
-			[Key.ALT]: Command.B,
-			[Key.CTRL]: Command.X,
-			[Key.SHIFT]: Command.Y,
-			[Key.ENTER]: Command.START,
-			[Key.BACK]: Command.SELECT,
-		};
 
 		this.onKey(event, true);
 		addEventListener("keydown", event => this.onKey(event, true), false);
@@ -46,7 +24,7 @@ export class Keyboard {
 	}
 
 	onKey(event, isDown) {
-		const COMMAND = this.keyToCommandMap[event.keyCode];
+		const COMMAND = KeyMap[event.keyCode];
 		this._buffer[COMMAND] = isDown;
 
 		if (isDown && "number" == typeof(COMMAND)) {
